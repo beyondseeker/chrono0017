d270c9eface8676baf009987413a01e5a6e9b620
beyondseeker <makoto.sato@objectfanatics.com>
2020-08-06 06:29:15 +0900

    add Gradle Versions Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.github.ben-manes.versions") version "0.29.0"
+}
+
 buildscript {
     repositories {
         google()
