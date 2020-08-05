1050c13ee41c16858343b6e8a8d2312a6d82f1f7
beyondseeker <makoto.sato@objectfanatics.com>
2020-08-06 08:12:37 +0900

    configure Component Selection Rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id("com.github.ben-manes.versions") version "0.29.0"
 }
@@ -16,7 +18,6 @@ buildscript {
         // in the individual module build.gradle files
     }
 }
-
 allprojects {
     repositories {
         google()
@@ -26,4 +27,36 @@ allprojects {
 
 task("clean", Delete::class) {
     delete = setOf(rootProject.buildDir)
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+// Example for configuring Component Selection Rules.
+// @See https://docs.gradle.org/current/userguide/dynamic_versions.html#sec:component_selection_rules
+tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
+    // Example 1: reject all non stable versions
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+
+    // Example 2: disallow release candidates as upgradable versions from stable versions
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+
+    // Example 3: using the full syntax
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
 }
